5baa172add39e68c5d7d40ba573183dec3becc04
Nikola_K <140345347+N1koLa-99@users.noreply.github.com>
2024-11-06 14:40:13 +0200

    Update UserRepository.cs

--- WebApi/ClassRepository/UserRepository.cs
+++ WebApi/ClassRepository/UserRepository.cs
@@ -45,11 +45,6 @@ namespace SpoerStats2.ClassRepository
         {
             using (var connection = new SqlConnection(_connectionString))
             {
-                // В методите AddUser и UpdateUser използвайте един и същ код за хеширане:
-                var passwordHasher = new PasswordHasher<User>();
-                user.Password = passwordHasher.HashPassword(user, user.Password);
-
-
                 var query = "INSERT INTO Users (FirstName, LastName, Age, Email, Password, Gender, RoleID, ClubID, profileImage_url) " +
                             "VALUES (@FirstName, @LastName, @Age, @Email, @Password, @Gender, @RoleID, @ClubID, @profileImage_url)";
                 await connection.ExecuteAsync(query, new
@@ -66,31 +61,10 @@ namespace SpoerStats2.ClassRepository
                 });
             }
         }
-
         public async Task UpdateUser(User user)
         {
             using (var connection = new SqlConnection(_connectionString))
             {
-                var existingUser = await connection.QuerySingleOrDefaultAsync<User>("SELECT * FROM Users WHERE Id = @Id", new { Id = user.Id });
-                if (existingUser == null)
-                {
-                    throw new ArgumentException("User not found.");
-                }
-
-                // Ако паролата е предоставена, хешираме я
-                if (!string.IsNullOrWhiteSpace(user.Password))
-                {
-                    // В методите AddUser и UpdateUser използвайте един и същ код за хеширане:
-                    var passwordHasher = new PasswordHasher<User>();
-                    user.Password = passwordHasher.HashPassword(user, user.Password);
-
-                }
-                else
-                {
-                    // Ако паролата не е предоставена, оставяме същата парола (не променяме хеша)
-                    user.Password = existingUser.Password;
-                }
-
                 var query = "UPDATE Users SET FirstName = @FirstName, LastName = @LastName, Age = @Age, Email = @Email, " +
                             "Gender = @Gender, profileImage_url = @profileImage_url, Password = @Password WHERE Id = @Id";
 
@@ -101,7 +75,7 @@ namespace SpoerStats2.ClassRepository
                 parameters.Add("Email", user.Email);
                 parameters.Add("Gender", user.Gender);
                 parameters.Add("profileImage_url", user.profileImage_url);
-                parameters.Add("Password", user.Password); // Записваме хешираната парола
+                parameters.Add("Password", user.Password);
                 parameters.Add("Id", user.Id);
 
                 await connection.ExecuteAsync(query, parameters);
@@ -115,7 +89,6 @@ namespace SpoerStats2.ClassRepository
                 await connection.ExecuteAsync(query, new { Id = id });
             }
         }
-
         public async Task<User> GetUserByEmailAndPassword(string email, string password)
         {
             using (var connection = new SqlConnection(_connectionString))
