6688a908bc17f7bcbea15716046e5756db287a8e
Nikola_K <140345347+N1koLa-99@users.noreply.github.com>
2024-11-06 10:27:36 +0200

    Add files via upload

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,1 +1,87 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SpoerStats2.ClassRepository;
+using SpoerStats2.IRepository;
+using SpoerStats2.Repository;
+using SpoerStats2.Service;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
 
+namespace SpoerStats2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Register services and repositories
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IClubRepository, ClubRepository>();
+            builder.Services.AddScoped<IDisciplineRepository, DisciplineRepository>();
+            builder.Services.AddScoped<IResultRepository, ResultRepository>();
+            builder.Services.AddScoped<IRankingRepository, RankingRepository>();
+            builder.Services.AddScoped<INormativeRepository, NormativeRepository>();
+            builder.Services.AddScoped<IRankRepository, RankRepository>();
+            builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+            builder.Services.AddScoped<IClubDisciplineRepository, ClubDisciplineRepository>();
+
+            // Add Sport services and repositories
+            builder.Services.AddScoped<ISportRepository, SportRepository>();
+            builder.Services.AddScoped<SportService>();
+
+            // Register other services
+            builder.Services.AddScoped<RoleService>();
+            builder.Services.AddScoped<UserService>();
+            builder.Services.AddScoped<ClubService>();
+            builder.Services.AddScoped<DisciplineService>();
+            builder.Services.AddScoped<ResultService>();
+            builder.Services.AddScoped<RankingService>();
+            builder.Services.AddScoped<NormativeService>();
+            builder.Services.AddScoped<RankService>();
+            builder.Services.AddScoped<ClubDisciplineService>();
+
+            // Register DbConnection
+            builder.Services.AddScoped<IDbConnection>(sp =>
+                new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Configure CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors("AllowAll");
+
+            app.UseAuthentication();
+            app.UseAuthorization(); 
+
+            app.MapControllers();
+            app.Run();
+
+        }
+    }
+}
