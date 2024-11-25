5ef2c165783de2c77664a0a70f1c15b3c3c26a2d
Nikola_K <140345347+N1koLa-99@users.noreply.github.com>
2024-11-25 15:13:15 +0200

    Create UserSession.cs

---
+++ WebApi/Models/UserSession.cs
@@ -0,0 +1,42 @@
+
+namespace SpoerStats2.Models
+{
+    public class UserSession
+    {
+        public UserSession(User dbUser) 
+        {
+            Id = dbUser.Id;
+            FirstName=dbUser.FirstName;
+            LastName=dbUser.LastName;
+            Age=dbUser.Age;
+            Email=dbUser.Email;
+            Gender=dbUser.Gender;
+            RoleID=dbUser.RoleID;
+            ClubID=dbUser.ClubID;
+            profileImage_url=dbUser.profileImage_url;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int RoleID { get; set; }
+        public int ClubID { get; set; }
+        public string UserTokenHash {  get; set; }
+        public string profileImage_url { get; set; }
+
+        public string GetUserTokenHash()
+        {
+            string data = FirstName + LastName + Age + Email + Gender + RoleID + ClubID + profileImage_url + Id;
+
+            using (var sha256 = System.Security.Cryptography.SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(data));
+                return Convert.ToBase64String(bytes);
+            }
+
+        }
+    }
+}
